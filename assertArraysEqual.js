e088d961d1e2c948bb2de41342d1f4836307580e
alparslanustaa <alparslanustaa@gmail.com>
2022-01-12 22:46:27 +0000

    eqArrays add

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,3 +7,13 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+var eqArrays = function(arr1, arr2) {
+
+  for (var i = 0; arr1.length < i; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
