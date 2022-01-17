7d3ec2076f2b884f54fc18928430f792abb6dd18
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 22:36:22 +0000

    initial upload

--- without.js
+++ without.js
@@ -1,13 +1,5 @@
-const assertEqual = function (actual, expected) {
 
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-let without = function (source, itemsToRemove) {
+const without = function (source, itemsToRemove) {
   for (let i = 0; source.length > i; i++) {
     for (let j = 0; itemsToRemove > j; j++) {
       if (source[i] === itemsToRemove[j]) {
@@ -18,7 +10,4 @@ let without = function (source, itemsToRemove) {
   return source;
 }
 
-
-assertEqual(without([1, 2, 3], [1])) // => [2, 3]) // => [2, 3]
-assertEqual(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
+module.exports = without;
