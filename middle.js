958f9ac83598c3dc99dd50bb8812956968c943dc
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 21:52:15 +0000

    refactoring

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const assertEqual = function (actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-let eqArrays = function (arr1, arr2) {
-  for (var i = 0; arr1.length < i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function(arr){
   if(arr.length<=2){
       return [];
@@ -27,9 +9,4 @@ const middle = function(arr){
    return [arr[Math.floor(arr.length / 2)]];
   }
 }
-
-
-// assertEqual(middle([1])) // => []
-// assertEqual(middle([1, 2])) // => []
-// assertEqual(middle([1, 2, 3])) // => [2]
-// assertEqual(middle([1, 2, 3, 4,])) // => [3]
+module.exports = middle;
