0414b99e2179f50b1a01048b85e592723eadc442
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 22:13:25 +0000

    chai implement

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,9 @@
-const middle = require("../middle");
-const assertArraysEqual = require ("../assertArraysEqual")
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
- 
-//Test Code
-assertArraysEqual(middle([1])) // => []
-assertArraysEqual(middle([1, 2])) // => []
-assertArraysEqual(middle([1, 2, 3])) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4,])) // => [3]
+
+describe("#middle", () => { 
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+});
+});
