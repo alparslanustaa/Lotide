40b373bdded68254488989284a0728e1e829d10c
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 22:35:07 +0000

    initial upload

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const findKeyByValue = function(obj, val) {
   for (let key of Object.keys(obj)) {
@@ -17,11 +9,4 @@ const findKeyByValue = function(obj, val) {
   return undefined;
 }
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
