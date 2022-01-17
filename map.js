b1d8a9ffcb9f95de31d9f63ac4f867ec3d5b857d
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 22:35:50 +0000

    initial upload

--- map.js
+++ map.js
@@ -1,26 +1,3 @@
-
-
-const assertArrayEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-var eqArrays = function(arr1, arr2) {
-
-  for (var i = 0; arr1.length < i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -29,5 +6,5 @@ const map = function(array, callback) {
   return results;
 }
 
-const results1 = map(words, word => word[0]);
-assertArrayEqual(results1, ["g", "c", "t", "m", "t"])
+
+module.exports = map;
