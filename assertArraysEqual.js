4eda398c4499f6af8ca65ef15835d77d9206e2d9
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 21:44:50 +0000

    module export add

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,4 @@ const assertArrayEqual = function(actual, expected) {
   }
 };
 
-var eqArrays = function(arr1, arr2) {
-
-  for (var i = 0; arr1.length < i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+module.exports = assertArrayEqual;
