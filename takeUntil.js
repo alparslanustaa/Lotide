ebd6ddda635fcad7198b3bfe52a987ee89248ccc
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 22:36:01 +0000

    initial upload

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,3 @@
-
-const assertArrayEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-var eqArrays = function(arr1, arr2) {
-
-  for (var i = 0; arr1.length < i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const takeUntil = function(array, callback) {
   const result = [];
   for (const item of array) {
@@ -30,7 +10,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArrayEqual(takeUntil(data1, x => x < 0));
-assertArrayEqual(takeUntil(data2, x => x === ','));
+module.exports = takeUntil;
