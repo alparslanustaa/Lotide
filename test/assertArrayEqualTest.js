88f63ea368e21f052bddfa60b6dd7e357ec10e7d
alparslanustaa <alparslanustaa@gmail.com>
2022-01-17 21:44:05 +0000

    test code add

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require ("../assertArraysEqual")
+const eqArrays = require("../eqArrays");
+
+//Test code
+assertArraysEqual(eqArrays("Lighthouse Labs", "Bootcamp"));
+assertArraysEqual(eqArrays(1, 1));
